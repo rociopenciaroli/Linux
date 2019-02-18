4dcafe2743f2fa96c9b99c1d6f4ac0684aee4107
Alejandro De Castro <29775143+aledc7@users.noreply.github.com>
2019-02-17 21:21:04 -0300

    Create generate_ssl.sh
    Script para generar el certificado SSL para mi sitio aledc.com

---
+++ generate_ssl.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+
+function pause(){
+   read -p "$*"
+}
+
+
+pause ' script para la generacion de Certificado SSL -  Presione Enter para generar el certificado'
+
+cd acme-client/
+php bin/acme setup --server letsencrypt --email alejandrocdecastro@gmail.com
+
+php bin/acme issue --domains aledc.com:www.aledc.com --path /home/u174569164/public_html:/home/u174569164/public_html --server letsencrypt
+
+cd /home/u174569164/acme-client/data/certs/acme-v01.api.letsencrypt.org.directory/aledc.com
+
+
+pause  'generando certificao que va en el Primer bloque:'
+cat fullchain.pem
+
+function pause(){
+   read -p "$*"
+}
+pause 'generando  la KEY  que va en el Segundo bloque'
+
+cat key.pem
